7831e68ff45471abf296fc4ec014f91f2076a71f
Nuhash Touhid <Nuhash Touhid>
2017-03-12 22:13:24 -0400

    initial commit of Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,63 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class Rectangle extends Application{
+	public void start (Stage primaryStage){
+		
+		TextInputDialog dialog= new TextInputDialog();
+		dialog.setContentText("Enter the height of the rectangle");
+		dialog.showAndWait();
+		String height=dialog.getResult();
+		double dblheight=Double.parseDouble(height);
+		dialog.setContentText("Enter the width of the rectangle");
+		dialog.showAndWait();
+		String width=dialog.getResult();
+		double dblwidth=Double.parseDouble(width);
+
+		double area=dblheight*dblwidth;
+		double perimeter=2*(dblheight+dblwidth);
+
+		Alert result= new Alert(AlertType.INFORMATION);
+		result.setContentText("The area of the rectangle is "+area+"\nThe perimeter of the rectangle is "+perimeter);
+		result.showAndWait();
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
